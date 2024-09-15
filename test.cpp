1b55313792165cf63ba2507e5cc02c256d7e54db
nnfuad <nnfuad01@gmail.com>
2024-09-16 02:28:02 +0600

    test updated

--- test.cpp
+++ test.cpp
@@ -1,3 +1,15 @@
+// #include <iostream>
+// #include <cmath>
+// #include <string>
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// int main(){
+//     int x = 0;
+//     int y = 4;
+//     cout<<y/x;
+// }
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -6,7 +18,12 @@
 using namespace std;
 
 int main(){
-    int x = 0;
-    int y = 4;
-    cout<<y/x;
+    if(1)
+        for(int i= 0 ; i<3;i++)
+            cout<<i<<endl;
+        
+    else
+        cout<<"else block"<<endl;
+
+    
 }
