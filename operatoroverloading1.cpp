b03d9fa997ff970adab6a5fecbb9d00d80c50d36
nnfuad <nnfuad01@gmail.com>
2024-10-21 23:38:03 +0600

    added codes and mod one

--- operatoroverloading1.cpp
+++ operatoroverloading1.cpp
@@ -5,8 +5,23 @@
 #include <map>
 using namespace std;
 
-class Mobile{};
+class complex{
+    private:
+
+    public:
+        double real;
+        double img;
+        complex(double r, double i) : real(r), img(i){};
+
+        complex operator+( complex const &other){
+            return complex(this->real + other.real , this->img + other.img);
+        }
+
+};
 
 int main(){
-    
+    complex c1(7.0,8.1);
+    complex c2(7.0,8.1);
+    c2 = c2+c1;
+    cout<<c2.real<<" + "<<c2.img<<"i"<<endl;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,3 +1,15 @@
+// #include <iostream>
+// #include <cmath>
+// #include <string>
+// #include <vector>
+// #include <map>
+// using namespace std;
+
+// int main(){
+//     int x = 0;
+//     int y = 4;
+//     cout<<y/x;
+// }
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -5,86 +17,15 @@
 #include <map>
 using namespace std;
 
-class Game {
-private:
-    string status;
+int main(){
+    if(1)
+        for(int i= 0 ; i<3;i++)
+            cout<<i<<endl;
+        
+    else
+        cout<<"else block"<<endl;
 
-public:
-    Game() {
-        status = "Not started";
-    }
+    int a=4;
+    cout<<a++ + a++;
 
-    void start() {
-        if (status == "Started") {
-            cout << "The game is already started." << endl;
-        } else {
-            status = "Started";
-            cout << "Game has started." << endl;
-        }
-    }
-
-    void pause() {
-        if (status == "Started") {
-            status = "Paused";
-            cout << "Game is paused." << endl;
-        } else if (status == "Paused") {
-            cout << "The game is already paused." << endl;
-        } else {
-            cout << "Game is not started yet." << endl;
-        }
-    }
-
-    void end() {
-        if (status == "Started" || status == "Paused") {
-            status = "Ended";
-            cout << "Game has ended." << endl;
-        } else if (status == "Ended") {
-            cout << "The game is already ended." << endl;
-        } else {
-            cout << "Game is not started yet." << endl;
-        }
-    }
-
-    void displayStatus(){
-        cout << "Current game status: " << status << endl;
-    }
-};
-
-int main() {
-    Game game;
-    int choice;
-
-    do {
-        cout << "\n--- Game Menu ---" << endl;
-        cout << "1. Start Game" << endl;
-        cout << "2. Pause Game" << endl;
-        cout << "3. End Game" << endl;
-        cout << "4. Display Game Status" << endl;
-        cout << "5. Exit" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-
-        switch (choice) {
-        case 1:
-            game.start();
-            break;
-        case 2:
-            game.pause();
-            break;
-        case 3:
-            game.end();
-            break;
-        case 4:
-            game.displayStatus();
-            break;
-        case 5:
-            cout << "Exiting the program." << endl;
-            break;
-        default:
-            cout << "Invalid choice! Please try again." << endl;
-        }
-
-    } while (choice != 5);
-
-    return 0;
 }
