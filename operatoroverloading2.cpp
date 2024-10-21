dc2550975569e8ce5eee643ace4e316c5c3c9105
nnfuad <nnfuad01@gmail.com>
2024-10-22 01:28:03 +0600

    added codes

---
+++ operatoroverloading2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+class coord{
+    private:
+    int x,y;
+    public:
+    int xgetter(){
+        return x;
+        }
+    int ygetter(){
+        return y;
+        }
+    coord(int x1,int y1):  x(x1) , y(y1){};
+    coord operator+(coord const &other);
+
+};
+
+coord coord::operator+(coord const &other){
+    return coord(this->x + other.x , this->y  + other.y);
+}
+
+int main(){
+    coord x1(1,2),x2(2,3);
+    x1  = x1+x2;
+    cout<<"(x,y)=("<<x1.xgetter()<<","<<x1.ygetter()<<")"<<endl;;;;;;;;;
+
+}
